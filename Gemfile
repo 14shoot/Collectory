6f5db64f91fe4a8c515673a9a02590c633b3bacf
千代　就人 <chiyo@MacBookPro.lan>
2026-08-30 09:22:36 +0900

    投稿

--- Gemfile
+++ Gemfile
@@ -2,8 +2,8 @@ source "https://rubygems.org"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 8.1.3"
-# The modern asset pipeline for Rails [https://github.com/rails/propshaft]
-gem "propshaft"
+# Use Sprockets for Bootstrap + dartsass-sprockets
+gem "sprockets-rails"
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", ">= 2.1"
 # Use the Puma web server [https://github.com/puma/puma]
@@ -66,3 +66,6 @@ group :test do
 end
 
 gem "devise"
+
+gem "bootstrap", "~> 5.3.8"
+gem "dartsass-sprockets"
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,9 +21,32 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:category_id, :location, :body)
+    params.require(:post).permit(:category_id, :location, :body, :image)
   end
 end
